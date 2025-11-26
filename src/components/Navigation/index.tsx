ab91afb710a7eb58240ddc2354ec9bb5de88006c
Sunil Kanumula <sunil.kanumula@gmail.com>
2025-11-26 19:00:07 +0100

    added nav links

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,23 @@
+import Link from 'next/link';
+
 const Navigation = () => {
     return (
         <nav className="flex space-x-6">
-            <a href="/" className="text-gray-700 hover:text-blue-500 font-medium">Home</a>
-            <a href="/destinations" className="text-gray-700 hover:text-blue-500 font-medium">Destinations</a>
-            <a href="/tours" className="text-gray-700 hover:text-blue-500 font-medium">Tours</a>
-            <a href="/about" className="text-gray-700 hover:text-blue-500 font-medium">About Us</a>
-            <a href="/contact" className="text-gray-700 hover:text-blue-500 font-medium">Contact</a>
+            <Link href="/" className="text-gray-700 hover:text-blue-500 font-medium">
+                Home
+            </Link>
+            <Link href="/destinations" className="text-gray-700 hover:text-blue-500 font-medium">
+                Destinations
+            </Link>
+            <Link href="/tours" className="text-gray-700 hover:text-blue-500 font-medium">
+                Tours
+            </Link>
+            <Link href="/about" className="text-gray-700 hover:text-blue-500 font-medium">
+                About Us
+            </Link>
+            <Link href="/contact" className="text-gray-700 hover:text-blue-500 font-medium">
+                Contact
+            </Link>
         </nav>
     );
 }
